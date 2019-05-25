b9cfb323fc918318a7db7de6a46fa23fc5737a83
chuasweechin <sampiochua@gmail.com>
2019-05-25 11:26:08 +0800

    disable auto loading of script when page runs

--- js/content.js
+++ js/content.js
@@ -3,14 +3,11 @@
         let pwElement = document.querySelector("input[type='password']");
 
         if (pwElement === null) {
-        chrome.runtime.sendMessage(null);
-
-        } else if (pwElement.value.length === 0 ) {
             chrome.runtime.sendMessage(null);
-
+        } else if (pwElement.value.length === 0) {
+            chrome.runtime.sendMessage(null);
         } else if (pwElement.value.length < 6) {
             chrome.runtime.sendMessage("short");
-
         } else {
             chrome.runtime.sendMessage(pwElement.value);
         }
