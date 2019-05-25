7cb8e805d57367e2281a610c2240690ca322e159
chuasweechin <sampiochua@gmail.com>
2019-05-26 02:31:14 +0800

    improve styling, implement a more robust way to check DOM

--- js/content.js
+++ js/content.js
@@ -1,13 +1,33 @@
+function checkForValidPasswordField (elements) {
+    for (let i = 0; i < elements.length; i++) {
+        const elementStyle = window.getComputedStyle(elements[i]);
+
+        if (elementStyle.display !== "none" && elementStyle.visibility !== "hidden" &&
+                    (elementStyle.top.split('px')[0] >= 0 || elementStyle.top === "auto") &&
+                        (elementStyle.left.split('px')[0] >= 0 || elementStyle.left === "auto") &&
+                            (elementStyle.right.split('px')[0] >= 0 || elementStyle.right === "auto") &&
+                                (elementStyle.bottom.split('px')[0] >= 0 || elementStyle.bottom === "auto")) {
+            return elements[i];
+        }
+    }
+
+    return null;
+}
+
 (
     function () {
-        let pwElement = null
+        // scan DOM for password field
+        let pwElement = checkForValidPasswordField(document.querySelectorAll("input[type='password']"));
 
-        if (window.frames[0] === undefined) {
-            pwElement = document.querySelector("input[type='password']");
-        } else {
-            pwElement = window.frames[0].document.querySelector("input[type='password']");
+        if (pwElement === null && window.frames.length > 0) {
+            for (let i = 0; i < window.frames.length; i++) {
+                pwElement = checkForValidPasswordField(window.frames[0].document.querySelectorAll("input[type='password']"));
+
+                if (pwElement !== null) { break; }
+            }
         }
 
+        // return results
         if (pwElement === null) {
             chrome.runtime.sendMessage(null);
         } else if (pwElement.value.length === 0) {
--- js/popup.js
+++ js/popup.js
@@ -1,47 +1,48 @@
 const checkButton = document.getElementById('checkButton');
 
+chrome.runtime.onMessage.addListener(async function (message) {
+    if (message === null) {
+         alert("\u26A0  There is no password to check!");
+    } else if (message === "short") {
+         alert("\u26A0  Your password is too short!");
+    } else {
+        try {
+            let found = false;
+
+            const hash =  hexString(await digestMessage(message));
+            const partialHash = hash.substring(5, hash.length);
+
+            const response = await fetch(`https://api.pwnedpasswords.com/range/${ hash.substring(0, 5) }`);
+            const data = await response.text()
+            const result = data.split('\n');
+
+            for (let i = 0; i < result.length; i++) {
+                const [resultHash, resultAppearanceCount] = [...result[i].split(':')];
+
+                if (resultHash.toLowerCase() === partialHash.toLowerCase() ) {
+                    alert(`\u2620  Oh no, your password is not secure! It has already been seen ${ formatNumber(resultAppearanceCount.trim()) } times.\n\nThis password has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it now!`)
+                    found = true;
+
+                    break;
+                }
+            }
+
+            if (found === false) {
+                alert(`\u2714 Good news, your password is safe!`)
+            }
+        } catch (e) {
+            console.log("An error occur");
+        }
+
+    }
+});
+
+
 checkButton.onclick = function(element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           { file: 'js/content.js' }
       );
-    });
-
-    chrome.runtime.onMessage.addListener(async function (message) {
-        if (message === null) {
-             alert("\u26A0 No password to check!");
-        } else if (message === "short") {
-             alert("\u26A0 Your password is too short!");
-        } else {
-            try {
-                let found = false;
-
-                const hash =  hexString(await digestMessage(message));
-                const partialHash = hash.substring(5, hash.length);
-
-                const response = await fetch(`https://api.pwnedpasswords.com/range/${ hash.substring(0, 5) }`);
-                const data = await response.text()
-                const result = data.split('\n');
-
-                for (let i = 0; i < result.length; i++) {
-                    const [resultHash, resultAppearanceCount] = [...result[i].split(':')];
-
-                    if (resultHash.toLowerCase() === partialHash.toLowerCase() ) {
-                        alert(`\u2620 Oh no, your password is not secure! It has already been seen ${ formatNumber(resultAppearanceCount.trim()) } times.\n\nThis password has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it now!`)
-                        found = true;
-
-                        break;
-                    }
-                }
-
-                if (found === false) {
-                    alert(`\u2714 Good news, your password is safe!`)
-                }
-            } catch (e) {
-                console.log(e);
-            }
-
-        }
     });
 };
