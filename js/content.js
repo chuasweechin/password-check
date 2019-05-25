045ab373dd5f157b7afa1d35c85184604c548f0e
chuasweechin <sampiochua@gmail.com>
2019-05-25 10:31:30 +0800

    code clean up

--- js/content.js
+++ js/content.js
@@ -1,9 +1,18 @@
-if (document.querySelector("input[type='password']") === null
-    || document.querySelector("input[type='password']").value.length === 0 ) {
-    chrome.runtime.sendMessage(null);
-} else if (document.querySelector("input[type='password']").value.length < 6) {
-    chrome.runtime.sendMessage("short");
-} else {
-    chrome.runtime.sendMessage(document.querySelector("input[type='password']").value);
-    delete(element);
-}
+(
+    function () {
+        let pwElement = document.querySelector("input[type='password']");
+
+        if (pwElement === null) {
+        chrome.runtime.sendMessage(null);
+
+        } else if (pwElement.value.length === 0 ) {
+            chrome.runtime.sendMessage(null);
+
+        } else if (pwElement.value.length < 6) {
+            chrome.runtime.sendMessage("short");
+
+        } else {
+            chrome.runtime.sendMessage(pwElement.value);
+        }
+    }
+)();
